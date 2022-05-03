ad629b10b13621685f6abacee4c085c82e90d27b
David Cook <davidthomascook@live.co.uk>
2022-05-03 11:53:40 +0100

    Add RefreshServices and get destinations to ServicesViewModel.
    These can then be called from the View.

--- TfGM-API-Wrapper-iOS/Shared/ServicesViewModel.swift
+++ TfGM-API-Wrapper-iOS/Shared/ServicesViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class ServicesViewModel : ObservableObject {
+    
+    @Published var services = FormattedServices(destinations: [:], messages: [])
+    
+    @Published var stop: Stop? = nil
+    
+    @MainActor
+    func refreshServices() async {
+            self.services = try! await ServicesRequest().requestServices(tlaref: stop!.tlaref)
+    }
+    
+    func getDestinationsAlphabetical() -> [String] {
+        return Array(self.services.destinations.keys).sorted {
+            $0 < $1
+        }
+    }
+}
