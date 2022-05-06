2322087dd8ea1187659a1d9ddd25351bb3280e0c
David Cook <davidthomascook@live.co.uk>
2022-05-06 15:30:14 +0100

    Add Favourites implementation and Sections to StopDetail.
    The implementation for favourites should be moved to a view model.

--- TfGM-API-Wrapper-iOS/Shared/StopDetail.swift
+++ TfGM-API-Wrapper-iOS/Shared/StopDetail.swift
@@ -14,41 +14,77 @@ struct StopDetail: View {
     @State private var zoomed = false
     @State private var mapManager = MapManager()
     
+    @EnvironmentObject var favouritesStore: FavouriteStopStore
+    
     var body: some View {
-        VStack (alignment: .leading) {
-            Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: stop.latitude, longitude: stop.longitude), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))))
-                .aspectRatio(contentMode: zoomed ? .fill: .fit)
-                .onTapGesture {
-                    withAnimation {
-                        zoomed.toggle()
-                    }
-                }
-            if (!zoomed) {
+        List {
+            Section {
+                Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: stop.latitude, longitude: stop.longitude), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))))
+                    .aspectRatio(contentMode: .fill)
+            }
+            .aspectRatio(contentMode: .fill)
+            
+            Section{
                 NavigationLink (destination: ServicesView(stop: stop)) {
                     Label("View Live Departures", systemImage: "tram")
                         .padding()
                 }
-                Button {
-                    mapManager.openMapsFromStop(stop: stop)
-                } label: {
-                    Label("Open in Maps", systemImage: "map.fill")
-                }
-                .padding()
-                
-                NavigationLink (destination: NearPublicTransport(stop: stop)) {
-                    Label("Nearby", systemImage: "mappin.and.ellipse")
-                        .padding()
-                }
-                
-                Label(stop.street, systemImage: "car")
-                    .padding()
-                Label("Stop Zone: \(stop.stopZone)", systemImage: "tram")
+            }
+            Button {
+                mapManager.openMapsFromStop(stop: stop)
+            } label: {
+                Label("Open in Maps", systemImage: "map.fill")
+            }
+            .padding()
+            
+            NavigationLink (destination: NearPublicTransport(stop: stop)) {
+                Label("Nearby", systemImage: "mappin.and.ellipse")
                     .padding()
             }
+            
+            Label(stop.street, systemImage: "car")
+                .padding()
+            Label("Stop Zone: \(stop.stopZone)", systemImage: "tram")
+                .padding()
+            
+            Section{
+                if favouritesStore.stops.contains(self.stop) {
+                    Button(action: {
+                        // Remove stop from favourites
+                        
+                        favouritesStore.stops.removeAll {$0 == self.stop}
+                        FavouriteStopStore.save(favouriteStops: favouritesStore.stops) { result in
+                            if case .failure(let error) = result {
+                                fatalError(error.localizedDescription)
+                            }
+                        }
+                        
+                        
+                    }) {
+                        Label("Remove from favourites", systemImage: "star.slash")
+                            .foregroundColor(.red)
+                    }
+                    .padding()
+                    
+                } else {
+                    Button(action: {
+                        // Adds the stop to favourites
+                        favouritesStore.stops.append(self.stop)
+                        FavouriteStopStore.save(favouriteStops: favouritesStore.stops) { result in
+                            if case .failure(let error) = result {
+                                fatalError(error.localizedDescription)
+                            }
+                        }
+                    }) {
+                        Label("Add to favourites", systemImage: "star.fill")
+                    }
+                    .padding()
+                }
+            }
         }
-            .navigationTitle(stop.stopName)
-            .edgesIgnoringSafeArea(.bottom)
-            .padding(.all)
+        .navigationTitle(stop.stopName)
+        .edgesIgnoringSafeArea(.bottom)
+        .padding(.all)
     }
 }
 
