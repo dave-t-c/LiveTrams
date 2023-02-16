b4b902146b946edb3a2a59953b28fff110ea2b8d
David Cook <davidthomascook@live.co.uk>
2023-02-16 22:43:49 +0000

    create basic watchos app.
    This incorporates most watchOS compatible features from the standard ios app.

---
+++ LiveTramsMCR/LiveTramsMCR Watch App/Home.swift
@@ -0,0 +1,82 @@
+//
+//  Home.swift
+//  LiveTramsMCR Watch App
+//
+//  Created by David Cook on 16/02/2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct Home: View {
+    @State var stops: [Stop] = []
+    @State private var searchText = ""
+    
+    @StateObject private var favouritesStore = FavouriteStopStore()
+    
+    var searchResults: [Stop] {
+        if searchText.isEmpty {
+            return stops
+        } else {
+            return stops.filter { $0.stopName.contains(searchText)}
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                
+                Section(header: Text("Favourites")){
+                    ForEach(favouritesStore.stops.sorted {$0.stopName < $1.stopName}) { stop in
+                        NavigationLink(destination: StopDetail(stop: stop, stops: stops).environmentObject(favouritesStore)) {
+                            VStack(alignment: .leading) {
+                                Text(stop.stopName)
+                                Text(stop.street)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                
+                Section(header: Text("All Stops"))
+                {
+                    ForEach(searchResults.sorted { $0.stopName < $1.stopName }) { stop in
+                        NavigationLink(destination: StopDetail(stop: stop, stops: stops).environmentObject(favouritesStore)) {
+                            VStack(alignment: .leading) {
+                                Text(stop.stopName)
+                                Text(stop.street)
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                    }
+                }
+                
+            }
+            .searchable(text: $searchText)
+            .navigationTitle("Stops")
+            .onAppear(){
+                FavouriteStopStore.load { result in
+                    switch result {
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    case .success(let stops):
+                        favouritesStore.stops = stops
+                    }
+                    
+                }
+                
+                StopRequest().requestStops { (stops) in
+                    self.stops = stops
+                }
+            }
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ LiveTramsMCR/LiveTramsMCR Watch App/LiveTramsMCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTramsMCRApp.swift
+//  LiveTramsMCR Watch App
+//
+//  Created by David Cook on 16/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveTramsMCR_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
