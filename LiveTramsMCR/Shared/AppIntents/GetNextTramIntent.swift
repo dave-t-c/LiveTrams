1aad74b341900868e2485f69345cf8c91c27ba28
David Cook <davidthomascook@live.co.uk>
2023-04-04 21:57:17 +0100

    Add verification of stop and use tlaref in services request.

--- LiveTramsMCR/Shared/AppIntents/GetNextTramIntent.swift
+++ LiveTramsMCR/Shared/AppIntents/GetNextTramIntent.swift
@@ -6,6 +6,8 @@
 //
 
 import AppIntents
+import SwiftUI
+import OrderedCollections
 
 struct GetNextTramIntent: AppIntent {
     
@@ -13,30 +15,48 @@ struct GetNextTramIntent: AppIntent {
     
     static var description = IntentDescription("Get live trams for a given stop")
     
+    private let maxDestinationsToShow = 3
+    
     @Parameter(title: "Stop", optionsProvider: StopNameOptionsProvider())
     var stop: String?
     
-    func perform() async throws -> some IntentResult & ProvidesDialog {
+    func perform() async throws -> some IntentResult & ProvidesDialog & ShowsSnippetView {
         
         guard let stop = stop else {
             throw $stop.needsValueError("Which stop?")
         }
         
-        let serviceRequester = ServicesRequest()
-        let services = try await serviceRequester.requestDepartureBoardServices(tlaref: stop)
-        let trams = services.trams
+        let stops = try await StopRequest().requestStopsAsync()
         
-        if trams.isEmpty && services.messages.isEmpty {
+        if !stops.contains(where: {$0.stopName == stop}) {
+            let possibleStops = stops.filter {$0.stopName.contains(stop)}
+            let stopNames = possibleStops.map { $0.stopName }
+            throw $stop.needsDisambiguationError(among: stopNames)
+        }
+        
+        let providedStop = stops.first(where: {$0.stopName == stop})!
+        
+        let serviceRequester = ServicesRequest()
+        let services = try await serviceRequester.requestServices(tlaref: providedStop.tlaref)
+        
+        if services.destinations.isEmpty && services.messages.isEmpty {
             return .result(dialog: "There is currently no live service information for \(stop)")
         }
         
-        if trams.isEmpty && !services.messages.isEmpty {
+        if services.destinations.isEmpty && !services.messages.isEmpty {
             return .result(dialog: "There is currently no live service information for \(stop), however there is a service update: \(services.messages.first!)")
         }
         
-        let nextTram = trams.first!
+        let destinationCount: Int = services.destinations.count
+        let destinationsToShow: Int = destinationCount > maxDestinationsToShow ? maxDestinationsToShow : destinationCount
+        let orderedServices = ServicesHelper().getDestinationsAsOrderedDict(destinations: services.destinations, limit: destinationsToShow)
         
-        return .result(dialog: "The next tram from \(stop) is to \(nextTram.destination) in \(nextTram.wait) mins")
+        let firstDestination = orderedServices.keys.first!
+        let firstDestinationFirstWait = orderedServices[firstDestination]!.first!.wait
+        
+        let dialogString: IntentDialog = IntentDialog("Here are the next trams from \(stop). The first is to \(firstDestination) in \(firstDestinationFirstWait) minutes")
+        
+        return .result(dialog: dialogString, view: NextServicesSummaryView(orderedServices: orderedServices))
     }
     
 }
@@ -46,5 +66,33 @@ private struct StopNameOptionsProvider: DynamicOptionsProvider {
         let stopRequester = StopRequest()
         let stops = try await stopRequester.requestStopsAsync()
         return stops.map { $0.stopName }
+    }
+}
+
+private struct NextServicesSummaryView: View {
+    
+    var orderedServices: OrderedDictionary<String, [Tram]>
+    
+    var body: some View {
+        
+        Spacer()
+        ForEach(orderedServices.keys, id: \.self) { stopName in
+            let trams: [Tram] = orderedServices[stopName]!
+            let tramTimes = trams.map {$0.wait}
+            let formattedTramTimes = tramTimes.joined(separator: ", ") + " mins"
+            HStack {
+                Text(stopName)
+                    .font(.title3)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading)
+                
+                Text(formattedTramTimes)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing)
+            }
+            Spacer()
+        }
     }
 }
