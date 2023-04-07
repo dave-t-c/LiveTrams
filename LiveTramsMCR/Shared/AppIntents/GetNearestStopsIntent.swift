48009262a49551e4b4e6472bc99c92f105177f7e
David Cook <davidthomascook@live.co.uk>
2023-04-07 22:50:32 +0100

    Add basic location manager.
    This uses location while app is active to find near stops.

---
+++ LiveTramsMCR/Shared/AppIntents/GetNearestStopsIntent.swift
@@ -0,0 +1,8 @@
+//
+//  GetNearestStopsIntent.swift
+//  LiveTramsMCR (iOS)
+//
+//  Created by David Cook on 07/04/2023.
+//
+
+import Foundation
---
+++ LiveTramsMCR/Shared/Model/Maps/LocationManager.swift
@@ -0,0 +1,47 @@
+//
+//  LocationManager.swift
+//  LiveTramsMCR (iOS)
+//
+//  Created by David Cook on 07/04/2023.
+//
+
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    @Published var authorizationStatus: CLAuthorizationStatus
+    @Published var lastSeenLocation: CLLocation?
+        
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        locationManager = CLLocationManager()
+        authorizationStatus = locationManager.authorizationStatus
+        
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        self.requestPermission()
+    }
+    
+    func requestPermission() {
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func requestLocation() {
+        locationManager.requestLocation()
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        lastSeenLocation = locations.first
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+
+}
