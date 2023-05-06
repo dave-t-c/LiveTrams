ba08cfe8adaea5c93eb2228f29ff870ecb4f343e
David Cook <davidthomascook@live.co.uk>
2023-05-06 15:20:39 +0100

    Remove ipad specifics in stop detail

--- LiveTramsMCR/Shared/Views/Stops/StopDetail.swift
+++ LiveTramsMCR/Shared/Views/Stops/StopDetail.swift
@@ -37,96 +37,87 @@ struct StopDetail: View {
                                 RoundedRectangle(cornerRadius: 25, style: .continuous)
                                     .fill(color)
                                     .frame(height: 10)
-                                    
+                                
                             }
                             Spacer()
                         }
                     }
                     
-                    switch UIDevice.current.userInterfaceIdiom {
-                    case .pad :
-                            Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: stop.latitude, longitude: stop.longitude), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))))
-                            .frame(width: geometry.size.width * 0.95, height: geometry.size.height / 2)
-                            .cornerRadius(10)
-                        
-                        
-                    default:
-                        Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: stop.latitude, longitude: stop.longitude), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))))
-                            .aspectRatio(contentMode: .fit)
-                            .cornerRadius(10)
-                    }
-                }
-                .listRowBackground(Color.clear)
-                
-                Section{
-                    NavigationLink (destination: ServicesView(stop: stop)) {
-                        Label("View Live Departures", systemImage: "clock.fill")
-                            .padding()
-                    }
-                }
-                
-                Section{
-                    NavigationLink (destination: JourneyPlanView(initialOrigin: stop.stopName, stops: stops.lazy.map { ($0.stopName)})) {
-                        Label("Plan Journey", systemImage: "map.fill")
-                            .padding()
-                    }
-                }
-                
-                
-                Button {
-                    mapManager.openMapsFromStop(stop: stop)
-                } label: {
-                    Label("Open in Maps", systemImage: "arrow.turn.up.right")
-                }
-                .padding()
-                
-                NavigationLink (destination: NearPublicTransport(stop: stop)) {
-                    Label("Nearby", systemImage: "mappin.and.ellipse")
-                        .padding()
-                }
-                
-                Label(stop.street, systemImage: "car")
-                    .padding()
-                Label("Stop Zone: \(stop.stopZone)", systemImage: "tram")
-                    .padding()
-                
-                Section{
-                    if favouritesStore.stops.contains(self.stop) {
-                        Button(action: {
-                            // Remove stop from favourites
-                            
-                            favouritesStore.stops.removeAll {$0 == self.stop}
-                            FavouriteStopStore.save(favouriteStops: favouritesStore.stops) { result in
-                                if case .failure(let error) = result {
-                                    fatalError(error.localizedDescription)
-                                }
-                            }
-                            
-                            
-                        }) {
-                            Label("Remove from favourites", systemImage: "star.slash")
-                                .foregroundColor(.red)
-                        }
-                        .padding()
-                        
-                    } else {
-                        Button(action: {
-                            // Adds the stop to favourites
-                            favouritesStore.stops.append(self.stop)
-                            FavouriteStopStore.save(favouriteStops: favouritesStore.stops) { result in
-                                if case .failure(let error) = result {
-                                    fatalError(error.localizedDescription)
-                                }
-                            }
-                        }) {
-                            Label("Add to favourites", systemImage: "star.fill")
-                        }
-                        .padding()
-                    }
+                    Map(coordinateRegion: .constant(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: stop.latitude, longitude: stop.longitude), span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005))))
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(10)
                 }
             }
-            .navigationTitle(stop.stopName)
+            .listRowBackground(Color.clear)
+            
+            Section{
+                NavigationLink (destination: ServicesView(stop: stop)) {
+                    Label("View Live Departures", systemImage: "clock.fill")
+                        .padding()
+                }
+            }
+            
+            Section{
+                NavigationLink (destination: JourneyPlanView(initialOrigin: stop.stopName, stops: stops.lazy.map { ($0.stopName)})) {
+                    Label("Plan Journey", systemImage: "map.fill")
+                        .padding()
+                }
+            }
+            
+            
+            Button {
+                mapManager.openMapsFromStop(stop: stop)
+            } label: {
+                Label("Open in Maps", systemImage: "arrow.turn.up.right")
+            }
+            .padding()
+            
+            NavigationLink (destination: NearPublicTransport(stop: stop)) {
+                Label("Nearby", systemImage: "mappin.and.ellipse")
+                    .padding()
+            }
+            
+            Label(stop.street, systemImage: "car")
+                .padding()
+            Label("Stop Zone: \(stop.stopZone)", systemImage: "tram")
+                .padding()
+            
+            Section{
+                if favouritesStore.stops.contains(self.stop) {
+                    Button(action: {
+                        // Remove stop from favourites
+                        
+                        favouritesStore.stops.removeAll {$0 == self.stop}
+                        FavouriteStopStore.save(favouriteStops: favouritesStore.stops) { result in
+                            if case .failure(let error) = result {
+                                fatalError(error.localizedDescription)
+                            }
+                        }
+                        
+                        
+                    }) {
+                        Label("Remove from favourites", systemImage: "star.slash")
+                            .foregroundColor(.red)
+                    }
+                    .padding()
+                    
+                } else {
+                    Button(action: {
+                        // Adds the stop to favourites
+                        favouritesStore.stops.append(self.stop)
+                        FavouriteStopStore.save(favouriteStops: favouritesStore.stops) { result in
+                            if case .failure(let error) = result {
+                                fatalError(error.localizedDescription)
+                            }
+                        }
+                    }) {
+                        Label("Add to favourites", systemImage: "star.fill")
+                    }
+                    .padding()
+                }
+            }
         }
+        .navigationTitle(stop.stopName)
     }
 }
 private func GetRouteColors(stop: Stop) -> [Color] {
@@ -142,18 +133,18 @@ private func GetRouteColors(stop: Stop) -> [Color] {
 
 private func hexStringToUIColor (hex:String) -> Color {
     var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-
+    
     if (cString.hasPrefix("#")) {
         cString.remove(at: cString.startIndex)
     }
-
+    
     if ((cString.count) != 6) {
         return Color.gray
     }
-
+    
     var rgbValue:UInt64 = 0
     Scanner(string: cString).scanHexInt64(&rgbValue)
-
+    
     return Color(
         red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
         green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
