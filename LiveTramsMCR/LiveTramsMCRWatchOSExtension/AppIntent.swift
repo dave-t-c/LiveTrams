a495deb5bb265c30a9a392df7c61b8beb1f78231
David Cook <45901991+dave-t-c@users.noreply.github.com>
2024-01-14 21:58:13 +0000

    Remove unused entity query.

--- LiveTramsMCR/LiveTramsMCRWatchOSExtension/AppIntent.swift
+++ LiveTramsMCR/LiveTramsMCRWatchOSExtension/AppIntent.swift
@@ -8,41 +8,6 @@
 import WidgetKit
 import AppIntents
 
-struct StopQuery: EntityQuery {
-    func entities(for identifiers: [StopDetail.ID]) async throws -> [StopDetail] {
-        let stops = try await StopRequest().requestStopsAsync()
-        let stopDetails =  stops.map { stop in
-            return StopDetail(id: stop.tlaref.hashValue, tlaref: stop.tlaref, stopName: stop.stopName)
-        }
-
-        return stopDetails.filter { identifiers.contains($0.id)}
-    }
-
-    func suggestedEntities() async throws -> [StopDetail] {
-        let stops = try await StopRequest().requestStopsAsync()
-        return stops.map { stop in
-            return StopDetail(id: stop.tlaref.hashValue, tlaref: stop.tlaref, stopName: stop.stopName)
-        }
-    }
-
-    func defaultResult() async -> StopDetail? {
-        try? await suggestedEntities().first
-    }
-}
-
-struct StopDetail: AppEntity {
-    let id: Int
-    let tlaref: String
-    let stopName: String
-
-    static var typeDisplayRepresentation: TypeDisplayRepresentation = "Stop"
-    static var defaultQuery = StopQuery()
-
-    var displayRepresentation: DisplayRepresentation {
-        DisplayRepresentation(title: "\(stopName)")
-    }
-}
-
 struct ConfigurationAppIntent: WidgetConfigurationIntent {
     static var title: LocalizedStringResource = "Configuration"
     static var description = IntentDescription("This is an example widget.")
